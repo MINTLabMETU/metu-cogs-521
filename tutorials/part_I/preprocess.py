2af88bdd5e9d9c7ccffd24aefee94be01e9aa6d1
F. Öter <firatoter@pm.me>
2020-10-28 12:12:20 +0300

    tutorial part I included

---
+++ tutorials/part_I/preprocess.py
@@ -0,0 +1,18 @@
+# for reading file names in a directory
+import glob
+
+def read_and_store_input(input_path):
+    # initialize dictonary
+    data = {}
+    # 
+
+
+
+def main():
+    input_path = "./in/"
+    output_path = "./out/"
+
+    ? = read_and_store_input(input_path)
+
+if __name__ == "__main__":
+    main()
